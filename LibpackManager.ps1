0f7affb4b2d3a68782e074fb054729b7c4b53c90
zachgummow <zach.gummow@gmail.com>
2019-08-25 08:46:12 -0500

    Organized code into sections.

--- LibpackManager.ps1
+++ LibpackManager.ps1
@@ -1,7 +1,15 @@
+# Define Command-Line Parameters
 param ([string]$libpack = $null)
+
+# Begin Global Variables
 
 $CurrentVersion = $null
 $LatestVersion = $null
+$active = $True 
+
+# End Global Variables
+
+# Begin Helper Functions
 
 function Display-Header {
   Write-Output "Managing LibPack at: $libpack"
@@ -19,20 +27,23 @@ function Get-Input {
   return $userInput
 }
 
-if(!(Test-Path "$libpack\plugins\imageformats\qsvg.dll")) {
-  Write-Output "$libpack is not a valid LibPack. Please validate the directory and try again."
-  $libpack = $null
-}
-
-while(!$libpack){
-  $libpack = Read-Host "Please select a LibPack to manage:`n"
+function Verify-Libpack {
   if(!(Test-Path "$libpack\plugins\imageformats\qsvg.dll")) {
     Write-Output "$libpack is not a valid LibPack. Please validate the directory and try again."
     $libpack = $null
   }
 }
 
-$active = $True 
+# End Helper Functions
+
+# Begin Main
+
+Verify-Libpack
+
+while(!$libpack){
+  $libpack = Read-Host "Please select a LibPack to manage:`n"
+  Verify-Libpack
+}
 
 while($active) {
 
